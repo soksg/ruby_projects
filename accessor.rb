a6a9a10c65075b3496d9338a53183db8a4cd6167
EC2 Default User <sk2361ksg@gmail.com>
2023-01-06 07:58:58 +0000

    [Finish]アクセサメソッドの学習

--- accessor.rb
+++ accessor.rb
@@ -1,25 +1,35 @@
 # アクセサメソッド・・・インスタンスう変数の値をを読み書きするメソッドのこと
 
 class User
+
+  # アクセサメソッドを書かなくてもインスタンス変数の参照、書き込みができるようになる
+  # attr_accessor :name
+
+  # インスタンスを読み取り専用にする（書き込み不可にする）
+  # attr_reader :name
+
+  # インスタンスを書き込み専用にする（読み込み不可にする）
+  attr_writer :name
+
   def initialize(name)
 # インスタンス変数は、クラスの外部からは参照できない
     @name=name
   end
 
   # 参照用のメソッドを作成すると、クラスの外部でもインスタンス変数を参照できる。
-  def name
-    @name
-  end
+  # def name
+  #   @name
+  # end
 
-  # インスタンス変数を変更した場合、変更用の変数を定義する
-  def name=(value)
-    @name=value
-  end
+  # # インスタンス変数を変更した場合、変更用の変数を定義する
+  # def name=(value)
+  #   @name=value
+  # end
 end
 
 emma=User.new("Emma")
 # emma.@name
-puts emma.name
+# puts emma.name
 # インスタンス変数の内容を変更
 emma.name="Emily"
-puts emma.name
+# puts emma.name
