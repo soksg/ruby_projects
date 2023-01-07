3f5d7c2fb5881e85cc73f97fd215d4c0e518e9e6
EC2 Default User <sk2361ksg@gmail.com>
2023-01-07 06:27:25 +0000

    [Finish]モジュールの学習

--- module.rb
+++ module.rb
@@ -12,3 +12,22 @@
 
 # 関連するメソッドや定数などをまとめてグループ化したいだけの時に、
 # モジュールは手軽に使えて便利
+
+module Driver
+  def self.run
+    puts "Run"
+  end
+
+  def self.stop
+    puts "Stop"
+  end
+end
+
+Driver.run
+Driver.stop
+
+# インスタンスの作成・継承はできない
+driver=Driver.new
+
+module TaxDriver<Driver
+end
